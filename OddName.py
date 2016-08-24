6d0159b7969293c8d9713158ecdd05d92e5beee1
Nando <nando.retallickboundey@my.jcu.edu.au>
2016-08-25 08:25:13 +1000

    add name printing program

---
+++ OddName.py
@@ -0,0 +1,9 @@
+def main():
+
+    name = str(input("What is your name??"))
+
+    surname = str(input("What is your last name??"))
+
+    print("your name is: {} {}".format(name, surname))
+
+main()
